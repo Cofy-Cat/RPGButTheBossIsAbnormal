556adad1573e7e78d2c0266baadbfc87f9ded00d
Jason Tung <lokyinjason921@gmail.com>
2025-03-24 23:26:25 +0800

    Move for 1 inch

--- Assets/Script/Core/State/IdleState.cs
+++ Assets/Script/Core/State/IdleState.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using cfEngine.Util;
 using RPG.Core.Player;
 using UnityEngine;
@@ -9,13 +10,12 @@ namespace RPG.Core.State {
         }
         public override CharacterStateId Id => CharacterStateId.Idle;
         private PlayerStateMachine _stateMachine;
+        public override HashSet<CharacterStateId> Whitelist { get; } = new() { CharacterStateId.Move };
+        
         protected override void StartContext(StateParam param) {
             if (param is Param p) {
                 _stateMachine = p.sm;
-                var animationName = AnimationName.GetDirectional(AnimationName.Idle, _stateMachine.Controller.LastFaceDirection);
-            
-                _stateMachine.Controller.Animation.Play(animationName, true);
-                _stateMachine.Controller.Rigidbody.linearVelocity = Vector2.zero;
+                _stateMachine.Controller.SetVelocity(Vector2.zero);
             }
         }
     }
