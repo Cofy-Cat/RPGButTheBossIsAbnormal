b2e5d9474942e35a7fa59ed695dbc836f280f20d
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2025-03-24 09:08:52 +0800

    minor update and fix

--- Assets/Script/Core/CharacterStateMachine.cs
+++ Assets/Script/Core/CharacterStateMachine.cs
@@ -18,6 +18,9 @@ namespace cfEngine.Core {
         public Controller Controller;
         [SerializeField] private IdleState idleState;
         [SerializeField] private MoveState moveState;
+        
+        // In MonoBehaviour, you can't use constructor, use Awake() instead
+        // Also, in MonoStateMachine, state registration is automatically done in Awake(), no need to call RegisterState() manually
         public CharacterStateMachine() : base() {
             RegisterState(idleState);
             RegisterState(moveState);
